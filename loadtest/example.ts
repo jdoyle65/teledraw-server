b19ad0c2d9353b840f9cc2bd23c8dc8f40c5b5cc
Justin Doyle <justin.doyle@redspace.com>
2020-04-16 12:12:18 -0300

    build: compile for heroku

--- loadtest/example.ts
+++ loadtest/example.ts
@@ -1,25 +1,25 @@
 import { Room, Client } from "colyseus.js";
 
-export function requestJoinOptions (this: Client, i: number) {
-    return { requestNumber: i };
+export function requestJoinOptions(this: Client, i: number) {
+  return { requestNumber: i };
 }
 
 export function onJoin(this: Room) {
-    console.log(this.sessionId, "joined.");
+  console.log(this.sessionId, "joined.");
 }
 
-export function onMessage(this: Room, message) {
-    console.log(this.sessionId, "received:", message);
+export function onMessage(this: Room, message: any) {
+  console.log(this.sessionId, "received:", message);
 }
 
 export function onLeave(this: Room) {
-    console.log(this.sessionId, "left.");
+  console.log(this.sessionId, "left.");
 }
 
-export function onError(this: Room, err) {
-    console.log(this.sessionId, "!! ERROR !!", err.message);
+export function onError(this: Room, err: any) {
+  console.log(this.sessionId, "!! ERROR !!", err.message);
 }
 
-export function onStateChange(this: Room, state) {
-    console.log(this.sessionId, "new state:", state);
+export function onStateChange(this: Room, state: any) {
+  console.log(this.sessionId, "new state:", state);
 }

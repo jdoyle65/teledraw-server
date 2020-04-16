e8a426c88b16fbb8d466e7a416d57e6e2035a66d
Justin Doyle <justin.doyle@redspace.com>
2020-04-16 12:32:57 -0300

    build: prep heroku

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static("uploads"));
+app.use(express.static(__dirname + "/uploads"));
 
 const server = http.createServer(app);
 const gameServer = new Server({
